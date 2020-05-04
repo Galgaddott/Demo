624ce0385f4d0fec18f4ebc289194edcfb424558
galgaddott <924669102@qq.com>
2020-05-04 21:01:35 +0800

    第一次测试提交

---
+++ index/src/main/java/com/example/index/IndexApplication.java
@@ -0,0 +1,13 @@
+package com.example.index;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndexApplication.class, args);
+    }
+
+}
