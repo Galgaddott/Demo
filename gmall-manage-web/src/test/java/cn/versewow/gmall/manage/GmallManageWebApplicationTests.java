efbd8b3a101c2acff9238f7da10a9ef829f6d088
galgaddott <924669102@qq.com>
2020-05-09 13:22:58 +0800

    管理系统Web层模块

---
+++ gmall-manage-web/src/test/java/cn/versewow/gmall/manage/GmallManageWebApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.versewow.gmall.manage;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallManageWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
