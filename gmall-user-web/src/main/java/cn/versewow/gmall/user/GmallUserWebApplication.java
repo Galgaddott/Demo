8cb8870a0a8f0545ecc3ab9af438fe191a6a9d79
galgaddott <924669102@qq.com>
2020-05-09 13:26:00 +0800

    客户Web层

--- gmall-user-web/src/main/java/cn/versewow/gmall/user/GmallUserWebApplication.java
+++ gmall-user-web/src/main/java/cn/versewow/gmall/user/GmallUserWebApplication.java
@@ -1,13 +1,13 @@
-package com.example.index;
+package cn.versewow.gmall.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IndexApplication {
+public class GmallUserWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IndexApplication.class, args);
+        SpringApplication.run(GmallUserWebApplication.class, args);
     }
 
 }
