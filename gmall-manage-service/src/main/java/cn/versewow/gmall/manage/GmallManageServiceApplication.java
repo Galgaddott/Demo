1989d448335879d101f4fd4dbfe844c3f0d4f7cb
galgaddott <924669102@qq.com>
2020-05-09 13:21:25 +0800

    管理系统Service层模块

---
+++ gmall-manage-service/src/main/java/cn/versewow/gmall/manage/GmallManageServiceApplication.java
@@ -0,0 +1,15 @@
+package cn.versewow.gmall.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages = "cn.versewow.gmall.manage.mapper")
+public class GmallManageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallManageServiceApplication.class, args);
+    }
+
+}
