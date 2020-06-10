22849840a336ac7b58fcdf858e7d211a0cf29521
galgaddott <924669102@qq.com>
2020-06-10 11:01:18 +0800

    搜索服务

---
+++ gmall-search-web/src/test/java/cn/versewow/gmall/search/GmallSearchWebApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.versewow.gmall.search;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallSearchWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
